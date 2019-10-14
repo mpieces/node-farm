d79e899253beaba46eee7acf113be24883bf73ec
Marisa Ramon <mer0522@gmail.com>
2019-10-14 15:41:43 -0500

    fill in templates with json data

--- index.js
+++ index.js
@@ -14,20 +14,52 @@ const url = require('url');
 
 ////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+    // wrap in regex and use global g
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
 
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
  const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+ // an array of 5 objects
+ // will loop through using .map
  const dataObj = JSON.parse(data);
+
 
  const server = http.createServer((req, res) =>  {
      const pathName = req.url;
 
+// OVERVIEW PAGE
      if(pathName === '/' || pathName === '/overview') {
-         res.end('This is the OVERVIEW');
+         
+         res.writeHead(200, { 'Content-type': 'text/html'})
+
+         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        //  console.log(cardsHtml);
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+         res.end(output);
+
+// PRODUCT PAGE
      } else if (pathName === '/product') {
          res.end('This is the PRODUCT');
+
+// API
      } else if (pathName ==='/api') {
         res.writeHead(200, { 'Content-type': 'application/json'})
         res.end(data);
+
+// NOT FOUND
      } else {
          // specify an object with the headers you want to send
          res.writeHead(404, {
