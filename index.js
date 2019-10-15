e0554a639c0af9623eed00c4851562fc8c95e182
Marisa Ramon <mer0522@gmail.com>
2019-10-15 15:38:18 -0500

    add own module and require in index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 //synchronous or blocking code execution
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -12,20 +14,7 @@ const url = require('url');
 
 ////////////////////////////
 // SERVER
-const replaceTemplate = (temp, product) => {
-    // wrap in regex and use global g
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
 
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
@@ -40,10 +29,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
     //  const pathName = req.url;
 
 // OVERVIEW PAGE
-     if(pathname === '/' || pathname === '/overview') {
-         
+     if(pathname === '/' || pathname === '/overview') { 
          res.writeHead(200, { 'Content-type': 'text/html'})
-
          const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
         //  console.log(cardsHtml);
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
@@ -54,7 +41,6 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
          // simplest way of retrieving an object based on a query string
          const product = dataObj[query.id];
          const output = replaceTemplate(tempProduct, product);
-
          res.end(output);
 
 // API
