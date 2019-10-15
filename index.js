37d4ea2006a86cf83d46d3350559b9e1050039fb
Marisa Ramon <mer0522@gmail.com>
2019-10-15 16:37:42 -0500

    install slugify and nodemon packages

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
+
+// FILES //////////////////////
 
 //synchronous or blocking code execution
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -18,52 +22,63 @@ const replaceTemplate = require('./modules/replaceTemplate');
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
- const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
- // an array of 5 objects
- // will loop through using .map
- const dataObj = JSON.parse(data);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+// an array of 5 objects
+// will loop through using .map
+const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map(el => slugify(el.productName, {
+    lower: true
+}));
+console.log(slugs);
 
- const server = http.createServer((req, res) =>  {
-     const { query, pathname } = url.parse(req.url, true);
+const server = http.createServer((req, res) => {
+    const {
+        query,
+        pathname
+    } = url.parse(req.url, true);
     //  const pathName = req.url;
 
-// OVERVIEW PAGE
-     if(pathname === '/' || pathname === '/overview') { 
-         res.writeHead(200, { 'Content-type': 'text/html'})
-         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    // OVERVIEW PAGE
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
         //  console.log(cardsHtml);
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-         res.end(output);
+        res.end(output);
 
-// PRODUCT PAGE
-     } else if (pathname === '/product') {
-         // simplest way of retrieving an object based on a query string
-         const product = dataObj[query.id];
-         const output = replaceTemplate(tempProduct, product);
-         res.end(output);
+        // PRODUCT PAGE
+    } else if (pathname === '/product') {
+        // simplest way of retrieving an object based on a query string
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
 
-// API
-     } else if (pathname ==='/api') {
-        res.writeHead(200, { 'Content-type': 'application/json'})
+        // API
+    } else if (pathname === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        })
         res.end(data);
 
-// NOT FOUND
-     } else {
-         // specify an object with the headers you want to send
-         res.writeHead(404, {
+        // NOT FOUND
+    } else {
+        // specify an object with the headers you want to send
+        res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello-world'
-         })
-         res.end('<h1>Page not found!</h1>');
-     }
+        })
+        res.end('<h1>Page not found!</h1>');
+    }
 
 
 
 
-     res.end('Hello from the server');
- });
+    res.end('Hello from the server');
+});
 
- server.listen(8000, '127.0.0.1', () => {
-     console.log('Listening to requests on port 8000');
- })
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+})
